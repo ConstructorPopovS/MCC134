412d4a7d900707ebb70936eee89c0f4b9ae7e6ed
Simeon <constructor.popovs@gmail.com>
2023-03-17 21:38:42 +0000

    save time data for each channelReading

--- simple_read_data.py
+++ simple_read_data.py
@@ -4,8 +4,8 @@ from time import sleep
 from sys import stdout
 from daqhats import mcc134, HatIDs, HatError, TcTypes
 from daqhats_utils import select_hat_device, tc_type_to_string
-# __import__('daqhats_utils')
-
+import csv #for saving data in fil in csv format
+import time #for creating timer during experiment
 
 # Constants
 CURSOR_BACK_2 = '\x1b[2D'
@@ -14,11 +14,16 @@ ERASE_TO_END_OF_LINE = '\x1b[0K'
 
 def main():
     tc_type = TcTypes.TYPE_K   # change this to the desired thermocouple type
-    delay_between_reads = 1  # Seconds
+    delay_between_reads = 0.1  # Seconds
     channels = (0, 1, 2, 3)
+    
 
-    # Creating a file with fileName#
+    # open the file in the write mode
     dataFile = open('file_From_Python.txt', 'w')
+
+    # create the csv writer
+    writer = csv.writer(dataFile)
+
 
     try:
         # Get an instance of the selected hat device object.
@@ -27,39 +32,62 @@ def main():
 
         for channel in channels:
             hat.tc_type_write(channel, tc_type)
-
+        
         # Display the header row for the data table.
-        print('\n  Sample', end='')
-        dataFile.write('Sample')
+        dataHeader = "Sample,"
+        # print('\n  Sample', end='')
         for channel in channels:
-            print('     Channel', channel, end='')
-            dataFile.write('     Channel')
-            dataFile.write(str(channel))
-        print('')
-        dataFile.write('\n')
+            # print('     Channel', channel, end='')
+            dataHeader += " Channel" + str(channel) + " Data" + ","
+            dataHeader += " Channel" + str(channel) + " Time" + ","
+        print(dataHeader)
+        dataFile.write(dataHeader + '\n')
 
         samples_per_channel = 0
-        while (samples_per_channel <= 10):
+        startTime = float(0.0)
+        
+        while (samples_per_channel <= 200):
+            dataRow = []
+            dataRow.append(samples_per_channel)
             # Display the updated samples per channel count
             print('\r{:8d}'.format(samples_per_channel), end='')
-            dataFile.write(str(samples_per_channel))
-            samples_per_channel += 1
+
+            if samples_per_channel == 0:
+                print('IF', end='\n')
+                startTime = time.perf_counter()
 
             # Read a single value from each selected channel.
             for channel in channels:
                 value = hat.t_in_read(channel)
+                channelTime = time.perf_counter()
+                channelTimeFromStart = float(channelTime) - float(startTime)
+                
+                dataRow.append(value)
+                dataRow.append(channelTimeFromStart)
+
+
                 if value == mcc134.OPEN_TC_VALUE:
                     print('     Open     ', end='')
+                    # writer.writerow('Open')
                 elif value == mcc134.OVERRANGE_TC_VALUE:
                     print('     OverRange', end='')
+                    # writer.writerow('OverRange')
                 elif value == mcc134.COMMON_MODE_TC_VALUE:
                     print('   Common Mode', end='')
+                    # writer.writerow('CommonMode')
                 else:
                     print('{:12.2f} C'.format(value), end='')
+                    strValue = str(value)
+                    # writer.writerow(strValue)
 
+                print('{:12.2f} s'.format(channelTimeFromStart), end='')
+
+            writer.writerow(dataRow)
+            samples_per_channel += 1
             # stdout.flush()
             # Wait the specified interval between reads.
             sleep(delay_between_reads)
+        print('\n')
         dataFile.close()
         
 
